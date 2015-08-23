58eb437489c5b5297d01295ac157f43f091da080
tushar091 <saha.tushar091@gmail.com>
2015-08-24 00:58:49 +0530

    commit

--- app/src/main/java/com/cquiz/confirmPop.java
+++ app/src/main/java/com/cquiz/confirmPop.java
@@ -1,15 +1,18 @@
 package com.cquiz;
 
+import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by tushar on 8/17/2015.
  */
 public class confirmPop extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
